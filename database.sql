80d28581cbdce3aa2c7a2107f755efafece1bf1f
Anirudh Subbarao <asubbarao450@gmail.com>
2024-07-22 15:17:23 -0500

    added code for database.sql

--- database.sql
+++ database.sql
@@ -2,8 +2,26 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
-CREATE TABLE "user" (
+CREATE TABLE "preferences" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
+);
+
+
+CREATE TABLE "preferences" (
+    "user_id" INT,
+    "notifications_email" BOOLEAN,
+    "notifications_sms" BOOLEAN,
+    "notifications_push" BOOLEAN,
+    "notifications_freq" VARCHAR(120),
+    "updated_at" timestamp
+);
+
+
+CREATE TABLE "faq" (
+    "id" INTEGER,
+    "question" VARCHAR(12000),
+    "answer" VARCHAR(12000)
+    
 );
