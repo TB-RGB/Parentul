5fd51210d3cc612d10ff09555df59be01c5d3c0e
Anirudh Subbarao <asubbarao450@gmail.com>
2024-07-22 15:14:26 -0500

    added code for faq router

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ const passport = require('./strategies/user.strategy');
 // Route Includes
 const userRouter = require('./routes/user.router');
 const preferencesRouter = require('./routes/preferences.router');
+const faqRouter = require('./routes/faq.router');
 
 // Express Middleware
 app.use(express.json());
@@ -30,6 +31,7 @@ app.use(passport.session());
 // Routes
 app.use('/api/user', userRouter);
 app.use('/api/preferences', preferencesRouter);
+app.use('/api/faq', faqRouter);
 
 // Listen Server & Port
 app.listen(PORT, () => {
